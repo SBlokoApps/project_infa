d17e7cde5bf1fb36667cd03dedb9e7ec20425bd6
SBloko <mailbloko@gmail.com>
2018-12-06 19:38:28 +0300

    Лень писать описание, что-то поделал

--- file_system.py
+++ file_system.py
@@ -4,25 +4,42 @@ import os
 class FileSystem:
     def __init__(self, base_path):
         self.path = base_path
+        self.forms = {}
+        with open('set.txt', 'r') as f:
+            for line in f:
+                i = line.split()
+                self.forms[i[0]] = i[1:]
 
     def vpered(self, name):
         self.path += '/'
         self.path += name
 
     def nazad(self):
-        self.path = '/'.join(self.path.split('/')[:-1])
+        try:
+            self.path = '/'.join(self.path.split('/')[:-1])
+            return True
+        except Exception:
+            return False
 
     def copy(self, name, new_dir):
-        pass
+        try:
+            self.copy(self.path + '/' + name, new_dir)
+            return True
+        except Exception:
+            return False
 
     def delete(self, name):
-        pass
+        try:
+            os.remove(self.path + '/' + name)
+            return True
+        except Exception:
+            return False
 
     def move(self, name, new_dir):
         if self.copy(name, new_dir):
             if self.delete(name):
                 return True
-            return False
+        return False
 
     def rename(self, name, new_name):
         poln_name_1 = self.path + '/' + name
@@ -34,16 +51,19 @@ class FileSystem:
             return False
 
     def vse(self):
-        spisok = 0
-        self.filter(spisok)
+        spisok = os.listdir(self.path)
+        return self.preobr(spisok)
 
-    def rejim(self, rejim):
+    def rejim(self, rej):
+        with open('set.txt', 'r') as f:
+            
+
+    def preobr(self, old_sp):
+        new_sp = [abc.split('/')[-1] for abc in old_sp]
+        new_sp2 = []
         pass
 
-    def filter(self, sp):
-        return sp
-
-    def new_papka(self, name):
+    def new_dir(self, name):
         try:
             os.mkdir(self.path + '/' + name)
             return True
@@ -53,5 +73,5 @@ class FileSystem:
     def new_txt(self, name):
         pass
 
-    def zapusk(self, name):
+    def open(self, name):
         os.startfile(self.path + '/' + name)
