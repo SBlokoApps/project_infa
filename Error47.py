1679c06522056d4068d9cf3bc44ffb0e8f396877
SuperBloko <mailbloko@gmail.com>
2018-12-11 08:25:53 +0300

    Версия 1.0

--- Error47.py
+++ Error47.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+# Version 1.0
 # Form implementation generated from reading ui file 'Error#47.ui'
 #
 # Created by: PyQt5 UI code generator 5.11.3
--- file_system.py
+++ file_system.py
@@ -1,3 +1,4 @@
+# Version 1.0
 import os
 import win32api
 import shutil
